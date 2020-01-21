c7e3a7d9fca29b183f2cb24594618c05d3a186ec
Bruno Gabriel da Silva <38823062+bgsilvait@users.noreply.github.com>
2020-01-21 23:14:24 +0000

    Update Enablessh_user_and_key.ps1

--- Enablessh_user_and_key.ps1
+++ Enablessh_user_and_key.ps1
@@ -10,19 +10,20 @@ Function is_elevated{
     If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
     [Security.Principal.WindowsBuiltInRole] "Administrator")) {
         Write-warning "This script requires elevated privileges to change Install Windows Features and change files."
-        Write-Host "Please re-launch the Powershell as Administrator." -foreground "red" -background "black"
+        Write-Host "Please re-launch the Powershell Session as Administrator." -foreground "red" -background "black"
         break
     }
 }
 Function install_ssh{
     try {
-        Write-Host "Installing SSHd and Set Powershell as default Session"
+        Write-Host "Installing SSHd and Set Powershell as default Session" -foregroundcolor "yellow"
         Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
         Set-Service sshd -StartupType Automatic
         Set-Service ssh-agent -StartupType Automatic
         Start-Service sshd
         Start-Service ssh-agent
         New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
+        Get-Service *ssh*
         Write-Host "OK" -foregroundcolor "green"
     }
     catch {
@@ -39,7 +40,7 @@ Function install{
 }
 
 if ($RunMode -eq "Test1"){
-    Write-Host "Running Default(Test1) Mode" -background "black"
+    Write-Host "Running Default(Test1) Mode" -foregroundcolor "green"
     install
 
 } else {
